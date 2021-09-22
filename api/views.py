effcccf00a27bdb1757aa3f382190f2effcd0539
jayantkishore <jayantkishore1998@gmail.com>
2021-09-22 22:12:21 +0530

    added rest framework

--- api/views.py
+++ api/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+import json
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+import requests
+from wsgiref.util import FileWrapper
+from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'api/index.html', context=None)
